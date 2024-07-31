61735733b236e9411345f9d3a55f5a2e5689e955
Pierre Wieser <p.wieser@trychlos.org>
2024-07-31 18:39:39 +0200

    Post-release minimal version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'pwix:accounts-conf',
-    version: '1.0.1',
+    version: '1.0.2-rc',
     summary: 'Meteor accounts common configuration',
     git: 'https://github.com/trychlos/pwix-accounts-conf',
     documentation: 'README.md'
